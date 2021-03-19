a4ce1574a87f8bbe3e76ec225721f74251e623f3
kylegavinsimone <kylesimone@outlook.com>
2021-03-18 20:11:01 -0700

    formatting for mark down js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,25 +1,21 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if(license === "Apache-2.0")
-  return `<img src="https://img.shields.io/badge/License-Apache%202.0-blue.svg"></img>`
-  if(license === "ISC")
-  return `<img src="https://img.shields.io/badge/License-ISC-blue.svg"></img>`
-  if(license === "MIT")
-  return `<img src="https://img.shields.io/badge/License-MIT-yellow.svg"></img>`
-  
+  if (license === "Apache-2.0")
+    return `<img src="https://img.shields.io/badge/License-Apache%202.0-blue.svg"></img>`;
+  if (license === "ISC")
+    return `<img src="https://img.shields.io/badge/License-ISC-blue.svg"></img>`;
+  if (license === "MIT")
+    return `<img src="https://img.shields.io/badge/License-MIT-yellow.svg"></img>`;
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if(license === "Apache-2.0")
-  return "https://opensource.org/licenses/Apache-2.0"
-  if(license === "ISC")
-  return "https://opensource.org/licenses/ISC"
-  if(license === "MIT")
-  return "https://opensource.org/licenses/MIT"
-  
+  if (license === "Apache-2.0")
+    return "https://opensource.org/licenses/Apache-2.0";
+  if (license === "ISC") return "https://opensource.org/licenses/ISC";
+  if (license === "MIT") return "https://opensource.org/licenses/MIT";
 }
 
 // TODO: Create a function that returns the license section of README
@@ -28,8 +24,7 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  
-return `
+  return `
 ##${data.title}
 \n\n
 ${renderLicenseBadge(data.license)}
